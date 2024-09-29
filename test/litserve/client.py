e980ed88503c73517d56009333a02f0274d08ba5
sisconsavior <aceliuchanghong@gmail.com>
2024-09-29 09:34:42 +0800

    test: git

--- test/litserve/client.py
+++ test/litserve/client.py
@@ -1,26 +1,21 @@
-# Copyright The Lightning AI team.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# export no_proxy="localhost,127.0.0.1"
 import requests
 
 # 图像的URL和文本输入
-image_url = "https://picsum.photos/id/237/536/354"
-text_input = "以markdown表格形式描述图片"
+image_path = "https://picsum.photos/id/237/536/354"
+text_input = "what shown is that pic?"
+
+
+# image_path = "z_using_files/pics/11.jpg"
+# text_input = "以markdown表格还原图片表格文字"
+
+# image_path = "z_using_files/pics/00006737.jpg"
+# text_input = "图片中条形码是多少?"
 
 # 发送POST请求
 response = requests.post(
     "http://127.0.0.1:8927/predict",
-    json={"image_url": image_url, "text_input": text_input}
+    json={"image_path": image_path, "text_input": text_input}
 )
 
 # 打印状态码和响应内容
